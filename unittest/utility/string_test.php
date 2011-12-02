f34aafcac6ce16172121c24bf95313bf826d3ee4
Codelab <codelab@localhost>
2011-12-02 20:46:21 +0700

    added pre initial utility\string test

--- unittest/utility/string_test.php
+++ unittest/utility/string_test.php
@@ -1,3 +1,26 @@
 <?php
 
 
+if( ! defined('DOCROOT') )
+{
+    require_once '/var/www/testlab/Faye/unittest/bootstrap.php';
+}
+
+class StringTest
+{
+    function testingHypenate()
+    {
+        $expected = array(
+            'Hello-World',
+            'This-is-a-string-unit-test',
+        );
+        
+        $data = array(
+            'Hello World',
+            'This is a string unit test',
+        );
+        
+        $this->assertTrue( new String instanceof 'String' );
+        
+    }
+}
